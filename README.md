8239472e38f15344dfb2992ae859b815ede608ec
Djothi Grondin <djothi@MacBook-Pro-de-Djothi.local>
2019-11-17 14:08:00 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# tweet_cleaner
+# Tweet Cleaner
+
+## Purpose
+This simple app intends to delete tweets that match a query
+
+## Configuration
+Define them for Twitter as environnement variable while running the script
+`CONSUMER_KEY`
+`CONSUMER_SECRET`
+`ACCESS_KEY`
+`ACCESS_SECRET`
+`USER`
+
+## Deployement
+To make it callable once every 48h, simply use crontab.
+```sh
+crontab -e
+# 0 2 */2 * * CONSUMER_KEY=<your_key> CONSUMER_SECRET=<your_key> ACCESS_KEY=<your_key> ACCESS_SECRET=<your_key> USER=<twitteruserid> node <path_to_script>/index.js
+````
---
+++ index.js
@@ -0,0 +1,21 @@
+const Twitter = require('twitter')
+
+client = new Twitter({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token_key: process.env.ACCESS_KEY,
+	access_token_secret: process.env.ACCESS_SECRET
+})
+
+client.get('search/tweets', {q: '#ISS #NASA #space #NoutPei'}, (error, tweets) => {
+	if(error) throw error;
+	const { statuses } = tweets
+	statuses.forEach(({ id_str, user: { id: userId }}) => {
+		if(`${userId}` === process.env.USER) {
+			client.post(`statuses/destroy/${id_str}`,  (error, tweet) => {
+				if(error) throw error
+				console.info(`Deleted tweet ${id_str}`)
+			})
+		}
+	})
+})
